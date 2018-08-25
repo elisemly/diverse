73fbf0f3834fce10b62d00069767dc9ba76b3867
elisemly <elisemly@student.matnat.uio.no>
2018-08-25 14:02:35 +0200

    this is a test for stupid people

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+hello = input("Hello WHAT MATE")
+
+if hello == "Hellow world".lower():
+    print("Move bitch get out the way")
+else:
+    print("I have failed you as a human being, as a friend, but first and foremost as a motherfucker")
