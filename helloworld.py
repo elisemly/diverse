6db7a3d840489254fe4183de7edfd5b2f9aed459
elisemly <elisemly@student.matnat.uio.no>
2018-08-25 14:07:11 +0200

    added some lyrics for maximum discomfort

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,10 @@
 hello = input("Hello WHAT MATE")
 
-if hello == "Hellow world".lower():
+if hello == "Hello world".lower():
     print("Move bitch get out the way")
 else:
     print("I have failed you as a human being, as a friend, but first and foremost as a motherfucker")
+
+hola = input("Que pasa senooooor \n")
+
+print("Despacito, quiero respirar tu cuello despacito ")
